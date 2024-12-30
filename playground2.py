fc1efcfb61b69c805f74d5a7600c00e220ed67c3
Ricardo Neto <neto.lx@gmail.com>
2024-12-30 12:38:06 +0000

    Code cleanup

--- playground2.py
+++ playground2.py
@@ -68,15 +68,7 @@ class NoGatheringProblemError(Exception):
     pass
 
 Path = tuple[str,]
-
 directions = {'U':Location(0,1), 'D':Location(0,-1), 'L':Location(-1,0), 'R':Location(1,0)}
-
-# Transforms state of a world in a dictionary[str(location), Thing]
-def getStateByLocation(w: World) -> State:
-    s = dict()
-    for val in w.state.values():
-        s[str(val.where)] = val
-    return s
 
 def copyGround(src: Ground) -> Ground:
     g = Ground(src.name, src.width, src.height)
@@ -113,6 +105,77 @@ def copyWorld(src: World) -> World:
             w.state[newThing.name] = newThing
         return w
 
+'''
+Checks if thingA and thingB are adjacent
+'''
+def isAdjacent (thingA:Thing, thingB:Thing) -> bool:
+    if (thingA.where.ypos == thingB.where.ypos):
+        if ((thingA.where.xpos+1 == thingB.where.xpos) or (thingA.where.xpos-1 == thingB.where.xpos)):
+            return True
+    if (thingA.where.xpos == thingB.where.xpos):
+        if ((thingA.where.ypos+1 == thingB.where.ypos) or (thingA.where.ypos-1 == thingB.where.ypos)):
+            return True
+    return False
+
+def checkAltDiff(w:World, locA:Location, locB:Location):
+    return abs(w.ground.space[locA.xpos][locA.ypos] - w.ground.space[locB.xpos][locB.ypos])
+
+def checkBounds(w:World, loc:Location) -> bool:
+    return (0 <= loc.ypos < w.ground.height) and (0 <= loc.xpos < w.ground.width)
+
+# Returns a list of Objects in a state dictionary
+def getObjectsInState(s:State) -> list[Object]:
+    return getThingsInStateByType(s, Object)
+
+# Returns a list of Agents in a state dictionary
+def getAgentsInState(s:State) -> list[Agent]:
+    return getThingsInStateByType(s, Agent)
+
+# Returns a list of instances of type 't' in a state dictionary
+def getThingsInStateByType(s:State, t:type) -> list[Thing]:
+    things = []
+    for thing in s.values():
+        if (isinstance(thing, t)):
+            things.append(thing)
+    return things
+
+# Transforms state of a world in a dictionary[str(location), Thing]
+def getStateByLocation(w: World) -> State:
+    s = dict()
+    for val in w.state.values():
+        s[str(val.where)] = val
+    return s
+
+def getAgent(w:World,n:str)->Agent:
+    return cast(Agent,w.state[n])
+
+def getObject(w:World,n:str)->Object:
+    return cast(Object,w.state[n])
+
+'''
+Prints the altitude of each cell in the console and 'A' for Agent, 'X' for Object or '.' for common wing
+'''
+def PrintWorld(w: World):
+    print()
+    s = getStateByLocation(w)
+    for col in range(w.ground.height):
+        for row in range(w.ground.width):
+            currLoc = Location(row, col)
+            if (str(currLoc) in s.keys()):
+                if (isinstance(s[str(currLoc)],Agent)):
+                    print("A", end="")
+                else:
+                    print("X", end="")
+            elif (w.ground.wormholes[row][col] != None):
+                print("W", end="")
+            elif (w.ground.commonWing[row][col]):
+                print(".", end="")
+            else:
+                print (w.ground.space[row][col], end="")
+        print()
+    return
+
+
 def newAgent(name:str) -> Agent:
     if (not name.replace("_", "").isalnum()) or (len(name) == 0):
         raise PlayGroundError
@@ -148,20 +211,16 @@ def setComWing (w:World, loc: Location, width:int, height:int) -> None:
             w.ground.commonWing[i][j] = True
 
 def setWormhole(w:World, loc1:Location, loc2:Location) -> None:
-
     if (not (checkBounds(w, loc1)) or not (checkBounds(w, loc2))):
         raise PlayGroundError
-    
     # Ensure wormhole ends are at least 2 positions apart
     if (abs(loc1.xpos-loc2.xpos) < 2 or abs(loc1.ypos-loc2.ypos) < 2):
         raise PlayGroundError
-    
     # Ensure wormhole ends are not adjacent to any other wormhole
     for dir in directions.values():
         for locToCheck in [Location(loc1.xpos + dir.xpos, loc1.ypos + dir.ypos), Location(loc2.xpos + dir.xpos, loc2.ypos + dir.ypos)]:
             if (checkBounds(w, locToCheck) and w.ground.wormholes[locToCheck.xpos][locToCheck.ypos] != None):
                 raise PlayGroundError
-    
     # Ensure wormhole ends don't land on any agent or object
     for thing in w.state.values():
         if (thing.where == loc1 or thing.where == loc2):
@@ -170,19 +229,6 @@ def setWormhole(w:World, loc1:Location, loc2:Location) -> None:
     # Add ends of the wormhole to the grid
     w.ground.wormholes[loc1.xpos][loc1.ypos] = loc2
     w.ground.wormholes[loc2.xpos][loc2.ypos] = loc1
-
-
-'''
-Checks if thingA and thingB are adjacent
-'''
-def isAdjacent (thingA:Thing, thingB:Thing) -> bool:
-    if (thingA.where.ypos == thingB.where.ypos):
-        if ((thingA.where.xpos+1 == thingB.where.xpos) or (thingA.where.xpos-1 == thingB.where.xpos)):
-            return True
-    if (thingA.where.xpos == thingB.where.xpos):
-        if ((thingA.where.ypos+1 == thingB.where.ypos) or (thingA.where.ypos-1 == thingB.where.ypos)):
-            return True
-    return False
 
 '''
 Checks if a Thing with the same name already exists in the world
@@ -202,14 +248,6 @@ def putThing (w:World, thing:Thing, loc:Location) -> Thing:
         thing.world = w
     return thing
 
-def checkAltDiff(w:World, locA:Location, locB:Location):
-    return abs(w.ground.space[locA.xpos][locA.ypos] - w.ground.space[locB.xpos][locB.ypos])
-
-def checkBounds(w:World, loc:Location) -> bool:
-    if (0 <= loc.ypos < w.ground.height) and (0 <= loc.xpos < w.ground.width):
-        return True
-    return False
-
 '''
 Moves the agent in the given direction
 Defines the agent's new location based on the supplied direction
@@ -219,9 +257,6 @@ Otherwise returns a copy of the world with the agent in the new location
 '''
 def moveAgent (w:World, ag:Agent,dir:str)-> World|None:
     newLocation = Location(ag.where.xpos + directions[dir].xpos, ag.where.ypos + directions[dir].ypos)
-
-    if not(checkBounds(w, newLocation)):
-        return None
 
     if (w.ground.wormholes[newLocation.xpos][newLocation.ypos] != None):
         whEnd = w.ground.wormholes[newLocation.xpos][newLocation.ypos]
@@ -275,7 +310,6 @@ def pathToWorlds (w:World, path: Path) -> list [World]:
                 worlds.append(currWorld)
     except:
         raise PlayGroundError()
-
     return worlds
 
 def nextWorlds(w:World) -> list[tuple[str,World]]:
@@ -293,16 +327,14 @@ def nextWorlds(w:World) -> list[tuple[str,World]]:
             # if an object is found and adjacent, picks object
             if isAdjacent(ag, ob):
                 nextWorld = pickObject(w, ag, ob)
-                if nextWorld != None:
-                    possibleWorlds.append(["P "+ ag.name +" "+ ob.name, nextWorld])
+                possibleWorlds.append(["P "+ ag.name +" "+ ob.name, nextWorld])
     return possibleWorlds
 
 '''
 Check if location to visit is within bounds, was not visited yet and meets altitude requirements
 '''
 def isValidMove(w:World, ag:Agent, loc:Location, visited):
-    x, y = loc.xpos, loc.ypos
-    if checkBounds(w, loc) and not visited[x][y]:
+    if checkBounds(w, loc) and not visited[loc.xpos][loc.ypos]:
         if checkAltDiff(w, ag.where, loc) <= (len(ag.objects)):
             return True
     return False
@@ -313,15 +345,15 @@ def getStateKey(s:State) -> str:
         res = res + k + "|" + str(s[k].where) + "|||"
     return res
 
-def findGoal(w:World, goal:Callable [[State],bool]) -> Path:
-    '''
-    Queue to store path and state needed to generate other worlds
-    Keep track of states already visited and to visit
+'''
+Queue to store path and state needed to generate other worlds
+Keep track of states already visited and to visit
 
-    For each element in queue:
-        generate new worlds and paths to get there
-        add to the queue current path and new path along with new state
-    '''
+For each element in queue:
+    generate new worlds and paths to get there
+    add to the queue current path and new path along with new state
+'''
+def findGoal(w:World, goal:Callable [[State],bool]) -> Path:
     toVisit:Queue[tuple[Path, State, str]] = Queue()
     stateKey = getStateKey(w.state)
     toVisit.put(((), w.state, stateKey))
@@ -350,26 +382,7 @@ def IsCookieFound_Goal(s:State)->bool:
     for agent in getAgentsInState(s):
         if (len(agent.objects) > 0):
             return True
-        # for object in agent.objects:
-        #     if (agent.where == object.where):
-        #         return True
     return False
-
-# Returns a list of Objects in a state dictionary
-def getObjectsInState(s:State) -> list[Object]:
-    return getThingsInStateByType(s, Object)
-
-# Returns a list of Agents in a state dictionary
-def getAgentsInState(s:State) -> list[Agent]:
-    return getThingsInStateByType(s, Agent)
-
-# Returns a list of instances of type 't' in a state dictionary
-def getThingsInStateByType(s:State, t:type) -> list[Thing]:
-    things = []
-    for thing in s.values():
-        if (isinstance(thing, t)):
-            things.append(thing)
-    return things
 
 def validCookieMonster(w:World) -> bool:
     if (w.ground.width * w.ground.height > 400):
@@ -435,105 +448,3 @@ def gatherWizards(w:World)->Path:
         finalPath.extend(res)
         w.state.pop(ag.name)
     return finalPath
-
-'''
-Print the world in the console
-If the cell is an Agent prints an 'A'
-If it's an Object prints an 'X'
-Otherwise prints the altitude of the cell
-'''
-def PrintWorld(w: World):
-    print()
-    s = getStateByLocation(w)
-    for col in range(w.ground.height):
-        for row in range(w.ground.width):
-            currLoc = Location(row, col)
-            if (str(currLoc) in s.keys()):
-                if (isinstance(s[str(currLoc)],Agent)):
-                    print("A", end="")
-                else:
-                    print("X", end="")
-            elif (w.ground.wormholes[row][col] != None):
-                print("W", end="")
-            elif (w.ground.commonWing[row][col]):
-                print(".", end="")
-            else:
-                print (w.ground.space[row][col], end="")
-        print()
-    return
-
-def getAgent(w:World,n:str)->Agent:
-    return cast(Agent,w.state[n])
-
-def getObject(w:World,n:str)->Object:
-    return cast(Object,w.state[n])
-
-'''
-Testing area
-'''
-# def g0(s:State)->bool:
-#     return cast(Agent, s["Ze"]).where == Location(2,0) and len(cast(Agent, s["Ze"]).objects) == 1
-
-
-# w = newWorld("Playground",3,3)
-# robot1 = newAgent("CookieMonster")
-# putThing(w,robot1,Location(0,0))
-# box0 = newObject("box0")
-# putThing(w,box0,Location(1,1))
-# box1 = newObject("box1")
-# putThing(w,box1,Location(2,2))
-# # box2 = newObject("box2")
-# # putThing(w,box2,Location(1,7))
-# # box3 = newObject("box3")
-# # putThing(w,box3,Location(1,19))
-# # setAltitude(w,Location(3,0),2,11,9) ## check this
-# # setAltitude(w,Location(3,12),2,8,9) ## check this
-# print(CookieMonster(w))
-
-
-# w:World = newWorld("S",3,1)
-# #PrintWorld(w)
-# a = newAgent("Ze")
-# putThing(w, a, Location(0,0))
-
-# o = newObject("Maria")
-# putThing(w, o, Location(1,0))
-
-# print(getStateKey(w.state))
-
-#PrintWorld(w)
-
-# res = findGoal(w, g0)
-# print(res)
-
-# b = newAgent("Quim")
-# putThing(w, b, Location(3,3))
-
-# c = newAgent("Tó")
-# putThing(w, c, Location(0,9))
-
-# d = newAgent("Manel")
-# putThing(w, d, Location(9,9))
-
-# # o1 = newObject("Sophie")
-# # putThing(w, o1, Location(7,4))
-
-# # o2 = newObject("Julie")
-# # putThing(w, o2, Location(7,7))
-
-# setComWing(w, Location(4,4), 4, 4)
-# setAltitude(w, Location(0,3), 1, 6, 9)
-# # setWormhole(w, Location(3,4), Location(5,7))
-# # setWormhole(w, Location(5,4), Location(7,7))
-
-# # wn = moveAgent(w, getAgent(w, "Quim"), "R")
-# # PrintWorld(wn)
-# # wn = moveAgent(wn, getAgent(wn, "Quim"), "R")
-# # PrintWorld(wn)
-# # print(getAgent(wn, "Quim").where)
-
-# res = gatherWizards(w)
-# print(res)
-
-# w2 = pathToWorlds(w, res)
-# PrintWorld(w2[len(w2)-1])
